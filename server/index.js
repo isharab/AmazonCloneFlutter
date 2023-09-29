d630613e9f4ec4a88a64e60e082bd1ed2bf549a8
Isharab Ahmed <isharab.dev@gmail.com>
2023-09-29 23:06:18 +0500

    started working on API

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log('Server-side code running');
+// CREATING AN API
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+// GET, POST, PUT, DELETE , UPDATE => CRUD
+app.get('/flutter-world', (req,  res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
